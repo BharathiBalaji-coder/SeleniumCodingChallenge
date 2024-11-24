2957fc0acb92c1e4ff85a65d1c498d76a8ae3594
Bharathi Balaji Rangaraj <76836867+BharathiBalaji-coder@users.noreply.github.com>
2024-11-24 12:18:30 +0530

    Commit Selenium Coding Challenge Day5

---
+++ src/main/java/org/example/MultipleClickMethodsTestCase2.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class MultipleClickMethodsTestCase2 {
+
+    public static void main(String[] args) {
+
+//step1 :Open your preferred web browser
+        WebDriver driver = new ChromeDriver();
+        //Step 2:Go to gemini.google.com
+        driver.get("https://gemini.google.com/");
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+        //click on sign in button
+        WebElement signIn = driver.findElement(By.xpath("//a[@aria-label='Sign in']"));
+        signIn.click();
+        String pageTitle = driver.getTitle();
+        System.out.println(pageTitle);
+        //Step 3: close the browser
+        driver.close();
+    }
+}
