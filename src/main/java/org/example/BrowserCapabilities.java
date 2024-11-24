13a552c3103a8d6a77e18b5618e25f13db5dbd2d
Bharathi Balaji Rangaraj <76836867+BharathiBalaji-coder@users.noreply.github.com>
2024-11-24 11:38:09 +0530

    Commit code

---
+++ src/main/java/org/example/BrowserCapabilities.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.util.Arrays;
+
+public class BrowserCapabilities {
+    public static void main(String[] args) {
+        //step1 :Open your preferred web browser
+        //Create an instance of ChromeOptions class
+        ChromeOptions options = new ChromeOptions();
+        //Step 2: Handle the notifications Add chrome switch to disable notification - "**--disable-notifications**"
+        options.addArguments("--disable-notifications");
+        //Step3 :
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        options.addArguments("window-size=1200,800");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.spicejet.com/");
+        //Step 3: Resize your browser window(1200,800)
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        //step 4: Remove info of chrome is being controlled by the software
+        options.setExperimentalOption("excludeSwitches", Arrays.asList("enable automation", "disable-infobars"));
+        //step 7:Get the text of flights
+        WebElement flightsText = driver.findElement(By.xpath("//div[text()='Flights']"));
+        String text = flightsText.getText();
+        System.out.println(text);
+        //Step 8: Print the title of the page
+        String pageTitle = driver.getTitle();
+        System.out.println(pageTitle);
+        //step 9 : Close the browser
+        driver.close();
+    }
+}
---
+++ src/main/java/org/example/ChromeBrowser.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ChromeBrowser {
+    public static void main(String[] args) {
+        //Launch Chrome browser in 3 different ways
+        //method1 : get
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.testleaf.com");
+        //method2: navigate
+        driver.navigate().to("https://www.testleaf.com");
+        //method3: ChromeOptions
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        options.addArguments("window-size=1200,800");
+        WebDriver driver1 = new ChromeDriver(options);
+        driver1.get("https://www.testleaf.com");
+        //method 4:
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ADMIN\\Documents\\chromedriver.exe");
+        // Instantiate a ChromeDriver class.
+        WebDriver driver2 = new ChromeDriver();
+        // Maximize the browser
+        driver2.manage().window().maximize();
+        // Launch Website
+        driver2.get("https://www.testleaf.com/");
+
+
+    }
+
+
+}
