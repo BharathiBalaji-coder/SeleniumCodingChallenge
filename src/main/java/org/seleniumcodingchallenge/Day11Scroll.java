fc601d1c3e4e79f61ef1fbbfc52e2e78683c1095
Bharathi Balaji Rangaraj <76836867+BharathiBalaji-coder@users.noreply.github.com>
2025-02-15 18:55:34 +0530

    Commit Selenium Coding Challenge

--- src/main/java/org/seleniumcodingchallenge/Day11Scroll.java
+++ src/main/java/org/seleniumcodingchallenge/Day11Scroll.java
@@ -1,9 +1,13 @@
 package org.seleniumcodingchallenge;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.awt.*;
 import java.time.Duration;
@@ -33,9 +37,13 @@ Step4: Quit the driver
         //Step 2 :implementation of javascript executor and click on the first result under key series
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+        // Step 2: Click on the first result under the Key Series
+        // Click on the first result under the Key Series
+        WebElement firstSeries = driver.findElement(By.xpath("//span[text()='India vs England']"));
+        firstSeries.click();
 
         //Step 4: Quit the driver
-        driver.quit();
+        // driver.quit();
 
     }
 }
